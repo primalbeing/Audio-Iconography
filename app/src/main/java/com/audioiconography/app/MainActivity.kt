92de474ce52e88aa32c28e469e170013c5fcb925
primalbeing <180481965+primalbeing@users.noreply.github.com>
2025-02-09 01:59:34 -0700

    functionality proven again

--- app/src/main/java/com/audioiconography/app/MainActivity.kt
+++ app/src/main/java/com/audioiconography/app/MainActivity.kt
@@ -2,7 +2,6 @@ package com.audioiconography.app
 
 import android.content.Context
 import android.content.Intent
-import android.media.AudioDeviceInfo
 import android.media.AudioManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -13,14 +12,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        val devices = audioManager.getDevices(AudioManager.GET_DEVICES_INPUTS)
-        val isWiredHeadsetConnected = devices.any {
-            it.type == AudioDeviceInfo.TYPE_WIRED_HEADSET || it.type == AudioDeviceInfo.TYPE_USB_HEADSET
-        }
-
-        if (isWiredHeadsetConnected) {
+        if (audioManager.isWiredHeadsetOn) {
             val serviceIntent = Intent(this, HeadphoneService::class.java)
-            startService(serviceIntent)
+            startForegroundService(serviceIntent)
         }
     }
 }
